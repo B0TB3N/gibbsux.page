54348ed6e2674ea85fbcb1e9040d14ce076a1b7a
Init0 <ben@atelierbiberen.ch>
2026-08-12 21:40:42 +0200

    more professional vibe

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+export const SERVER = {
+  name: "_Init0_'s Server",
+  address: 'jacobs-analogy.tun.ply.gg',
+  discord: 'https://discord.com/users/801135252206125096',
+  mcVersion: '1.21.8',
+  loader: 'Fabric',
+  loaderVersion: '0.19.3',
+  gamemode: 'Survival',
+  viewDistance: '10 chunks',
+  maxPlayers: 20,
+};
